dfa48cf25a68d315eac58985f6bd4182f10190d8
lucasgonzalez100 <gonzalezlucasnicolas69@gmail.com>
2022-10-17 19:19:04 -0300

    pasamos el proyecto al dom

--- main.js
+++ main.js
@@ -1,62 +1,64 @@
 
-let autoElegido = prompt ("selecione su auto"),
-  dni = prompt ("ingrese su dni"),
-monto = parseInt ( prompt  ("ingrese el monto a financiar")),
-cuota = parseInt ( prompt  ("ingrese la cantidad de cuotas "));
+//let autoElegido = prompt ("selecione su auto"),dni = prompt ("ingrese su dni"),monto = parseInt ( prompt  ("ingrese el monto a financiar")),cuota = parseInt ( prompt  ("ingrese la cantidad de cuotas "));
+
 const interes = 18;
 
+let monto = document.getElementById("monto")
+cuota = document.getElementById("cuotas")
+dni = document.getElementById("dni")
 
-//let monto =  document.getElementById("monto").value, cuota =  document.getElementById("cuotas").value, dni =   document.getElementById("dni").value;
+let calcular = document.getElementById("calcular")
 
-//let calcular = document.getElementById("calcular")
+calcular.addEventListener("click", accionBtn)
 
+function calculoInteres() {
+   financiacion = interes * cuota.value;
+   montoInteres = (financiacion / 100) * monto.value;
+   cuotaFinal = (montoInteres + parseFloat(monto.value)) / cuota.value;
+   montoCuotaConInteres = montoInteres + parseInt(monto.value);
 
-function calculoInteres(){
-   financiacion = interes*  cuota
-   montoInteres = (financiacion /100) *  monto
-   cuotaFinal= (montoInteres+monto )/cuota
-   montoCuotaConInteres= montoInteres+monto
-   
-   alert ("usted abonara " + cuota + " cuotas de " + cuotaFinal + " monto final " + montoCuotaConInteres )}
-  // let contenedorResultado = document.createElement("div");
+   alert("usted abonara " + cuota.value + " cuotas de " + cuotaFinal + " monto final " + montoCuotaConInteres)
+}
+ 
+function accionBtn () {
+if ((monto.value < 1) || (cuota.value < 1)) { alert("alguno de los valores ingresados es negativo o cero") }
 
-   //contenedorResultado.innerHTML= "<h3> usted abonara + cuotas + de + cuotaFinal </h3>"
+//else if ((monto.value === parseInt(monto.value)) && (cuota.value === parseInt(cuota.value))) { calculoInteres() }
 
-if ((monto == parseInt(monto) ) && (cuota == parseInt(cuota ) ))
+else {
 
-{calculoInteres() }
+  calculoInteres()
+}
+}
 
- else {
-   
-   alert ("alguno de los valores ingresados no es valido") }
+// let contenedorResultado = document.createElement("div");
 
-calcular.addEventListener ("click", calculoInteres)
+//contenedorResultado.innerHTML= "<h3> usted abonara + cuotas + de + cuotaFinal </h3>"
+
 
 
 class autos {
-   
-   constructor (id, marca,anio,version,color,kilometraje,estilo,precio ){
-      this.id = id ;    
+
+   constructor(id, marca, anio, version, color, kilometraje, estilo, precio) {
+      this.id = id;
       this.marca = marca;
       this.anio = anio;
       this.version = version;
       this.color = color;
       this.kilometraje = kilometraje;
       this.estilo = estilo;
-      this.precio = precio; 
+      this.precio = precio;
    }
 }
 
 
-   const auto1 = new autos( 01 ,"chevrolet", 2020, "cruze", "gris", 18000, "cashback", 3000000)
+const auto1 = new autos(01, "chevrolet", 2020, "cruze", "gris", 18000, "cashback", 3000000)
 
-   const auto2 = new autos(02 ,"renault", 2018, "clio", "blanco", 80000, "cashback", 1200000)
+const auto2 = new autos(02, "renault", 2018, "clio", "blanco", 80000, "cashback", 1200000)
 
-   
-   const autoUnico = {id:00, marca:"ford", anio:2015, version:"fiesta", color: "azul", kilometraje:50000, estilo:"sedan", precio:2500000 };
 
-   console.log(auto1)
-   console.log(auto2)
-   
- 
-   console.log(autoUnico)
+const autoUnico = { id: 00, marca: "ford", anio: 2015, version: "fiesta", color: "azul", kilometraje: 50000, estilo: "sedan", precio: 2500000 };
+
+console.log(auto1)
+console.log(auto2)
+console.log(autoUnico)
