fa83662fd4a6e1caa66b061a066b1b66694a3f9d
lucasgonzalez100 <gonzalezlucasnicolas69@gmail.com>
2022-10-25 23:49:46 -0300

    aun estoy trabajando en el localstorage para guardar datos

--- main.js
+++ main.js
@@ -4,57 +4,77 @@ const usuario = document.getElementById("usuario"),
    email = document.getElementById("email"),
    contraseña = document.getElementById("contraseña"),
    contraseña2 = document.getElementById("contraseña2"),
-  crearPerfil = document.getElementById("crearPerfil"),
-  alertas = document.getElementById("alarmas")
+   crearPerfil = document.getElementById("crearPerfil"),
+   alertas = document.getElementById("alarmas")
 
-crearPerfil.addEventListener("click",validacionPerfil )
+crearPerfil.addEventListener("click", validacionPerfil)
 
-   function validacionPerfil ( )
-   {
-    let alarmas =""
-    let entrar = false
-    let regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,4}))$/
-   
-    alertas.innerHTML = ""
+function validacionPerfil() {
+   let alarmas = ""
+   let entrar = false
+   let regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,4}))$/
 
+   alertas.innerHTML = ""
 
-    if (usuario.value.length <6)
-    {
-       alarmas += 'El nombre es muy corto <br>'
-       entrar = true
-    }  
+   if (usuario.value.length < 6) {
+      alarmas += 'El nombre es muy corto <br>'
+      entrar = true
+   }
 
-    if (dni.value < 7){
+   if (dni.value.length < 8) {
       alarmas += 'El dni es muy corta <br>'
       entrar = true
-   }  
-
-    if (!regexEmail.test(email.value)){
-      alarmas += 'El email es invalido <br>'
-      entrar= true
-    }
-
-    if (contraseña.value < 6){
-    alarmas += 'La contraseña es muy corta deben ser al menos 6 caracteres <br>'
-    entrar = true
- }  
-
- if (contraseña2.value != contraseña.value){
-    alarmas += "Las contraseñas no coinciden <br>"
-    entrar = true
- }
-
-    if (entrar){
-      alertas.innerHTML = alarmas
-    } 
-    else{
-      alertas.innerHTML = "perfil creado "
-    }
    }
+
+   if (!regexEmail.test(email.value)) {
+      alarmas += 'El email es invalido <br>'
+      entrar = true
+   }
+
+   if (contraseña.value < 6) {
+      alarmas += 'La contraseña es muy corta deben ser al menos 6 caracteres <br>'
+      entrar = true
+   }
+
+   if (contraseña2.value != contraseña.value) {
+      alarmas += "Las contraseñas no coinciden <br>"
+      entrar = true
+   }
+
+   if (entrar) {
+      alertas.innerHTML = alarmas
+   }
+   else {
+      alertas.innerHTML = "perfil creado "
+      // guardarUsuario ()
+   }
+}
+
+
+//   function guardarUsuario () {
+//     let user = {usuario: usuario.value, contraseña: contraseña.value };
+  
+//             if (valor === "sessionStorage") { 
+//                sessionStorage.setItem("item", JSON.stringify(user)); } 
+//            if (valor === "localStorage") { 
+//             localStorage.setItem("item", JSON.stringify(user)); 
+//              return user}
+
+//            function recuperarDatos() { 
+//             if ( (usuario.value = datos.usuario) && (contraseña.value = datos.contraseña) ) { 
+            
+//             (JSON.parse(localStorage.getItem("item")));
+//              sesionbtn.addEventListener("submit", (event) => {
+//                 console.log(event.target); 
+//                 event.preventDefault(); 
+//                if (checkbox.checked) { guardar("localStorage"); } 
+//                else { guardar("sessionStorage"); }});
+
+
+ingresoPerfil = document.getElementById ("ingresoPerfil")
 
 
 const interes = 18;
-
 let monto = document.getElementById("monto")
 cuota = document.getElementById("cuotas")
 calcular = document.getElementById("calcular")
@@ -62,38 +82,26 @@ contenedorDinamico = document.getElementById("contenedorDinamico")
 
 calcular.addEventListener("click", accionBtn)
 
-
 function calculoInteres() {
    financiacion = interes * parseFloat(cuota.value);
    montoInteres = (financiacion / 100) * monto.value;
    cuotaFinal = (montoInteres + parseFloat(monto.value)) / cuota.value;
    montoCuotaConInteres = montoInteres + parseInt(monto.value);
 
-  // contenedorDinamico.innerHTML = "usted abonara alguito" 
-
    salidaResultado = "usted abonara " + cuota.value + " cuotas de $" + parseInt(cuotaFinal) + " monto final $" + parseFloat(montoCuotaConInteres)
 
    contenedorDinamico.innerHTML = salidaResultado
-
-   //alert("usted abonara " + cuota.value + " cuotas de $" + parseInt(cuotaFinal) + " monto final $" + parseFloat(montoCuotaConInteres))
 }
 
 function accionBtn() {
    if ((monto.value < 1) || (cuota.value < 1)) {
-      
-      contenedorDinamico.innerHTML="algunos de los valores es 0 o negativo" }
 
-   else {
+      contenedorDinamico.innerHTML = "algunos de los valores es 0 o negativo"
+   }
 
-      calculoInteres()
+   else { calculoInteres()
    }
 }
-
-
-
-
-
-//" cuotas de $" ${ parseInt (cuotaFinal) }" monto final $" ${ parseFloat (montoCuotaConInteres)};
 
 class autos {
 
